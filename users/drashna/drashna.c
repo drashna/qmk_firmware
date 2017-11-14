8f788a85c74c35bf87fe14d6be6ff0b4636be2b4
Drashna Jael're <drashna@live.com>
2017-11-14 15:11:48 -0800

    Minor updates to keymaps

--- users/drashna/drashna.c
+++ users/drashna/drashna.c
@@ -1,3 +1,20 @@
+/*
+Copyright 2017 Christopher Courtney <drashna@live.com> @drashna
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "drashna.h"
 #include "quantum.h"
 #include "action.h"
@@ -17,12 +34,17 @@ __attribute__ ((weak))
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
+
 __attribute__ ((weak))
 uint32_t layer_state_set_keymap (uint32_t state) {
   return state;
 }
 
-bool is_overwatch = false;
+__attribute__ ((weak))
+void led_set_keymap(uint8_t usb_led) {}
+
+
+
 
 // Call user matrix init, set default RGB colors and then
 // call the keymap's init function
@@ -60,6 +82,13 @@ void matrix_init_user(void) {
 void matrix_scan_user(void) {
   matrix_scan_keymap();
 }
+
+void led_set_user(uint8_t usb_led) {
+  led_set_keymap(usb_led);
+}
+
+
+bool is_overwatch = false;
 
 #ifdef AUDIO_ENABLE
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
@@ -254,7 +283,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       unregister_code(is_overwatch ? KC_BSPC : KC_ENTER);
       _delay_ms(50);
       SEND_STRING("That aim is absolutely amazing. It's almost like you're a machine!" SS_TAP(X_ENTER));
-      _delay_ms(50);
+      _delay_ms(3000);
+      register_code(is_overwatch ? KC_BSPC : KC_ENTER);
+      unregister_code(is_overwatch ? KC_BSPC : KC_ENTER);
       SEND_STRING("Wait! That aim is TOO good!  You're clearly using an aim hack! CHEATER!" SS_TAP(X_ENTER));
     }
     return false;
