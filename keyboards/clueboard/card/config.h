3d2a2d6d464598e4a088aea954fc69f148dd8b6e
Drashna Jaelre <drashna@live.com>
2018-12-14 11:30:16 -0800

    Remove all existing definitions of IS_COMMAND if they use default LSHIFT and RSHIFT setting

--- keyboards/clueboard/card/config.h
+++ keyboards/clueboard/card/config.h
@@ -48,7 +48,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
- 
+
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCING_DELAY 20
 
@@ -95,11 +95,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * useful if your keyboard/keypad is missing keys and you want magic key support.
  *
  */
-
-/* key combination for magic key command */
-#define IS_COMMAND() ( \
-	keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
 
 /* control how magic key switches layers */
 //#define MAGIC_KEY_SWITCH_LAYER_WITH_FKEYS  true
