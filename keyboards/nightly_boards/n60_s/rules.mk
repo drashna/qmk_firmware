a7c2e51c0aedbc4298ee90ab094e2a8690235dfb
Drashna Jael're <drashna@live.com>
2021-05-10 11:23:29 -0700

    Disable audio on nightly_boards/n60_s as no pins are specified

--- keyboards/nightly_boards/n60_s/rules.mk
+++ keyboards/nightly_boards/n60_s/rules.mk
@@ -19,7 +19,7 @@ NKRO_ENABLE = yes             # USB Nkey Rollover
 BACKLIGHT_ENABLE = no         # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes         # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no         # Enable Bluetooth
-AUDIO_ENABLE = yes            # Audio output
+AUDIO_ENABLE = no             # Audio output
 ENCODER_ENABLE = yes          # Enable Rotary Encoders
 LTO_ENABLE = yes
 
