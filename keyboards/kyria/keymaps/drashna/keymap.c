385042c4d01dfbd1f1e84153abe8e434c04a0a4f
Drashna Jael're <drashna@live.com>
2021-06-13 21:02:29 -0700

    add more config and testing

--- keyboards/kyria/keymaps/drashna/keymap.c
+++ keyboards/kyria/keymaps/drashna/keymap.c
@@ -32,7 +32,7 @@
     K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
   ) \
   LAYOUT_wrapper( \
-      KC_ESC,  K01,     K02,     K03,     K04,     K05,                                             K06,     K07,     K08,     K09,     K0A,     KC_MINS, \
+      KC_ESC,  K01,     K02,     K03,     K04,     K05,    KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT,       K06,     K07,     K08,     K09,     K0A,     KC_MINS, \
    LALT_T(KC_TAB), K11, K12,     K13,     K14,     K15,                                             K16,     K17,     K18,     K19,     K1A, RALT_T(K1B), \
       OS_LSFT, CTL_T(K21), K22,  K23,     K24,     K25,  TG_GAME, MEH(KC_MINS),   TG_DBLO, KC_CAPS, K26,     K27,     K28,     K29, RCTL_T(K2A), OS_RSFT, \
                                  KC_MUTE, OS_LALT, KC_GRV,  KC_SPC,  BK_LWER,     DL_RAIS, KC_ENT,  OS_RGUI, UC(0x03A8), UC(0x2E2E) \
@@ -91,32 +91,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAMEPAD] = LAYOUT_wrapper(
-        KC_ESC,  KC_K,    KC_Q,    KC_W,    KC_E,    KC_R,                                          _______, _______, _______, _______, _______, _______,
+        KC_ESC,  KC_K,    KC_Q,    KC_W,    KC_E,    KC_R,        KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT,                                          _______, _______, _______, _______, _______, _______,
         KC_TAB,  KC_G,    KC_A,    KC_S,    KC_D,    KC_F,                                          _______, _______, _______, _______, _______, _______,
         KC_LCTL, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    _______, _______,   _______, LALT(KC_PSCR), _______, _______, _______, _______, _______, _______,
                                   _______, MAGIC_TOGGLE_NKRO, KC_V,     KC_SPC,  KC_H,   _______, _______, _______, _______, _______
     ),
 
     [_DIABLO] = LAYOUT_wrapper(
-        KC_ESC,  KC_S,    KC_I,    KC_F,    KC_M,    KC_T,                                          KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,
+        KC_ESC,  KC_S,    KC_I,    KC_F,    KC_M,    KC_T,         KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT,                                         KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,
         KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_G,                                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_LCTL, KC_D3_1, KC_D3_2, KC_D3_3, KC_D3_4, KC_Z,    KC_J,    KC_L,        TG_DBLO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                            KC_DIABLO_CLEAR, KC_J,    KC_NO, SFT_T(KC_SPACE), ALT_T(KC_Q),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
     [_LOWER] = LAYOUT_wrapper(
-        KC_F12,  _________________LOWER_L1__________________,                                       _________________LOWER_R1__________________, KC_F11,
+        KC_F12,  _________________LOWER_L1__________________,      KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT,                                         _________________LOWER_R1__________________, KC_F11,
         _______, _________________LOWER_L2__________________,                                       _________________LOWER_R2__________________, KC_PIPE,
         _______, _________________LOWER_L3__________________, _______, _______,   _______, _______, _________________LOWER_R3__________________, _______,
                                    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT_wrapper(
-        KC_F12,  _________________RAISE_L1__________________,                                       _________________RAISE_R1__________________, KC_F11,
+        KC_F12,  _________________RAISE_L1__________________,       KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT,                                        _________________RAISE_R1__________________, KC_F11,
         _______, _________________RAISE_L2__________________,                                       _________________RAISE_R2__________________, KC_BSLS,
         _______, _________________RAISE_L3__________________, _______, _______,   _______, _______, _________________RAISE_R3__________________, _______,
                                    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT_wrapper(
-        KC_MAKE, _________________ADJUST_L1_________________,                                       _________________ADJUST_R1_________________, KC_RESET,
+        KC_MAKE, _________________ADJUST_L1_________________,         KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT,                                      _________________ADJUST_R1_________________, KC_RESET,
         VRSN,    _________________ADJUST_L2_________________,                                       _________________ADJUST_R2_________________, EEP_RST,
         _______, _________________ADJUST_L3_________________, HPT_TOG, HPT_FBK,   MG_NKRO, UC_MOD,  _________________ADJUST_R3_________________, TG_MODS,
                                    _______, _______, _______, KC_NUKE, _______,   _______, _______, _______, _______, _______
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //       _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
 //       _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______,
 //                                  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
-//     ),
+//     ),c
 };
 // clang-format on
 
@@ -134,26 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 #endif
 
-#ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) {
-        // Page up/Page down
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
-    return true;
-}
-#endif
+
 
 #ifdef RGBLIGHT_LAYERS
 const rgblight_segment_t PROGMEM shift_layers[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -192,3 +173,8 @@ void matrix_scan_keymap(void) {
         rgblight_set_layer_state(3, mods & MOD_MASK_GUI);
 }
 #endif
+
+void keyboard_pre_init_keymap(void) {
+    debug_enable=true;
+    debug_matrix=true;
+}
