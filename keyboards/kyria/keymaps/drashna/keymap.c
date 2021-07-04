aa775888c08a81063570a1eb7d5c1a6df0c603c5
Drashna Jael're <drashna@live.com>
2021-07-04 10:31:29 -0700

    Update encoder mapping

--- keyboards/kyria/keymaps/drashna/keymap.c
+++ keyboards/kyria/keymaps/drashna/keymap.c
@@ -102,9 +102,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
 //     ),
 };
-// clang-format on
 
-const uint16_t PROGMEM encoder_map[][2][2] = {[_QWERTY] = {{KC_VOLD, KC_VOLU}, {KC_DOWN, KC_UP}}, [_RAISE] = {{KC_VOLD, KC_VOLU}, {KC_PGDN, KC_PGUP}}, [_LOWER] = {{RGB_MOD, RGB_RMOD}, {RGB_HUD, RGB_HUI}}, [_ADJUST] = {{CK_DOWN, CK_UP}, {KC_TRNS, KC_TRNS}}};
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [_QWERTY] = { { KC_VOLD, KC_VOLU }, { KC_DOWN, KC_UP   } },
+    [_RAISE]  = { { KC_VOLD, KC_VOLU }, { KC_PGDN, KC_PGUP } },
+    [_LOWER]  = { { RGB_MOD, RGB_RMOD },{ RGB_HUD, RGB_HUI } },
+    [_ADJUST] = { { CK_DOWN, CK_UP   }, { KC_TRNS, KC_TRNS } }
+};
+// clang-format on
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_keymap(oled_rotation_t rotation) { return OLED_ROTATION_180; }
--- layouts/community/ortho_4x12/drashna/keymap.c
+++ layouts/community/ortho_4x12/drashna/keymap.c
@@ -110,9 +110,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
-// clang-format on
 
-const uint16_t PROGMEM encoder_map[][1][2] = {[_QWERTY] = {{KC_DOWN, KC_UP}}, [_RAISE] = {{KC_VOLD, KC_VOLU}}, [_LOWER] = {{RGB_MOD, RGB_RMOD}}, [_ADJUST] = {{CK_DOWN, CK_UP}}};
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [_QWERTY] = { { KC_DOWN, KC_UP   } },
+    [_RAISE]  = { { KC_VOLD, KC_VOLU } },
+    [_LOWER]  = { { RGB_MOD, RGB_RMOD} },
+    [_ADJUST] = { { CK_DOWN, CK_UP   } }
+};
+// clang-format on
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
