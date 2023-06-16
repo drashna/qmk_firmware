8461f1c3e633b8280ffab8242f10d8215c263768
Drashna Jael're <drashna@live.com>
2023-06-15 22:39:00 -0700

    [Keyboard] Add micromod stuff

---
+++ keyboards/handwired/onekey/micromod/stm32f405/config.h
@@ -0,0 +1,26 @@
+// Copyright 2022 Stefan Kerkmann
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define QMK_WAITING_TEST_BUSY_PIN A15
+// #define QMK_WAITING_TEST_YIELD_PIN GP9
+
+#define I2C_DRIVER I2CD2
+#define I2C1_SDA_PIN B11
+#define I2C1_SCL_PIN B10
+
+#define I2C1_CLOCK_SPEED  400000
+#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_16_9
+
+#define BACKLIGHT_PWM_DRIVER PWMD8
+#define BACKLIGHT_PWM_CHANNEL 2
+
+#define AUDIO_PIN B4
+#define AUDIO_PWM_DRIVER PWMD3
+#define AUDIO_PWM_CHANNEL 1
+
+#define ADC_PIN C5
+
+#define ENCODERS_PAD_A { B7 }
+#define ENCODERS_PAD_B { B6 }
