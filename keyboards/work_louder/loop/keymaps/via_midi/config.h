c7f7c5723aa2d5a703c20dce9a7abf8a5e989ad4
Drashna Jael're <drashna@live.com>
2023-06-27 20:24:00 -0700

    [Keyboard] Add midi keymap for Work Louder boards

---
+++ keyboards/work_louder/loop/keymaps/via_midi/config.h
@@ -0,0 +1,17 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define MIDI_BASIC
+#define NO_ACTION_ONESHOT
+
+
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_TWINKLE
+
+#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
+#undef ENABLE_RGB_MATRIX_PIXEL_FLOW
+#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+
+#undef PRODUCT_ID
+#define PRODUCT_ID 0x2DF9
