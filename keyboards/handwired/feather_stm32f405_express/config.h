99b92177df3752f85f4601eb1df9caac8744c136
Drashna Jael're <drashna@live.com>
2023-12-30 04:04:02 -0800

    [Keyboard] Add handwired feather stm32f405 express board

---
+++ keyboards/handwired/feather_stm32f405_express/config.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Drashna Jael're (@Drashna Jael're)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define I2C1_CLOCK_SPEED  400000
+#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_16_9
+
+#define WS2812_PIN C0
+
+#define OLED_DISPLAY_128X128
+#define OLED_UPDATE_INTERVAL 100
+
+#define DRV2605L_FB_ERM_LRA   0
+#define DRV2605L_GREETING     DRV2605L_EFFECT_750_MS_ALERT_100
+#define DRV2605L_DEFAULT_MODE DRV2605L_EFFECT_BUZZ_1_100
