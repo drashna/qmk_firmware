6d65da94cfbd1e2f334da3dbc787bab5350dbb0a
Drashna Jael're <drashna@live.com>
2024-01-15 21:43:23 -0800

    [RGB Matrix] Use rgb matrix hsv to rgb function in all animations

--- quantum/rgb_matrix/animations/hue_breathing_anim.h
+++ quantum/rgb_matrix/animations/hue_breathing_anim.h
@@ -11,7 +11,17 @@ hsv_t HUE_BREATHING_math(hsv_t hsv, uint8_t i, uint8_t time) {
 }
 
 bool HUE_BREATHING(effect_params_t* params) {
-    return effect_runner_i(params, &HUE_BREATHING_math);
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+    uint8_t  huedelta = 12;
+    hsv_t    hsv      = rgb_matrix_config.hsv;
+    uint16_t time     = scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8);
+    hsv.h             = hsv.h + scale8(abs8(sin8(time) - 128) * 2, huedelta);
+    rgb_t rgb           = rgb_matrix_hsv_to_rgb(hsv);
+    for (uint8_t i = led_min; i < led_max; i++) {
+        RGB_MATRIX_TEST_LED_FLAGS();
+        rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+    }
+    return rgb_matrix_check_finished_leds(led_max);
 }
 
 #    endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
