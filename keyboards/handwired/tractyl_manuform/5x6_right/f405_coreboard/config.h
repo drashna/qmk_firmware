6c391d039fbd74ef524849969f13909c00d5a788
Drashna Jael're <drashna@live.com>
2024-07-23 09:28:27 -0700

    [TractylM] Completely redo F405 pin config

--- keyboards/handwired/tractyl_manuform/5x6_right/f405_coreboard/config.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/f405_coreboard/config.h
@@ -19,28 +19,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define USB_VBUS_PIN        B10
-#define SPLIT_HAND_PIN      A15  // high = left, low = right
+#define SPLIT_HAND_PIN      B11  // high = left, low = right
 #define USER_BUTTON_PIN     C13
 #define DEBUG_LED_PIN       B2
 
 // WS2812 RGB LED strip input and number of LEDs
-#define WS2812_PWM_DRIVER        PWMD1
-#define WS2812_PWM_CHANNEL       2
-#define WS2812_PWM_PAL_MODE      1
-#define WS2812_PWM_DMA_STREAM    STM32_DMA2_STREAM5
-#define WS2812_PWM_DMA_CHANNEL   6
+#define WS2812_PWM_DRIVER        PWMD8
+#define WS2812_PWM_CHANNEL       1
+#define WS2812_PWM_PAL_MODE      3
+#define WS2812_PWM_DMA_STREAM    STM32_DMA2_STREAM1
+#define WS2812_PWM_DMA_CHANNEL   7
 #define WS2812_PWM_TARGET_PERIOD 800000
-#define WS2812_PWM_COMPLEMENTARY_OUTPUT
 #define WS2812_EXTERNAL_PULLUP
 
 #define BACKLIGHT_PWM_DRIVER     PWMD3
-#define BACKLIGHT_PWM_CHANNEL    4
+#define BACKLIGHT_PWM_CHANNEL    2
 #define BACKLIGHT_PAL_MODE       2
 
 /* Audio config */
 #define AUDIO_PIN               A4
 #define AUDIO_PIN_ALT           A5
-#define AUDIO_POWER_CONTROL_PIN B11
+#define AUDIO_POWER_CONTROL_PIN A3
 #define AUDIO_PIN_ALT_AS_NEGATIVE
 
 /* serial.c configuration for split keyboard */
@@ -60,27 +59,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define I2C1_DUTY_CYCLE   FAST_DUTY_CYCLE_16_9
 
 /* spi config for eeprom and pmw3360 sensor */
-#define SPI_DRIVER        SPID2
-#define SPI_SCK_PIN       B13
+#define SPI_DRIVER        SPID1
+#define SPI_SCK_PIN       B3
 #define SPI_SCK_PAL_MODE  5
-#define SPI_MOSI_PIN      B15
+#define SPI_MOSI_PIN      B5
 #define SPI_MOSI_PAL_MODE 5
-#define SPI_MISO_PIN      B14
+#define SPI_MISO_PIN      B4
 #define SPI_MISO_PAL_MODE 5
 
 
 /* pmw3360 config  */
-#define POINTING_DEVICE_CS_PIN C6
+#define POINTING_DEVICE_CS_PIN B8
 #define PMW33XX_SPI_DIVISOR 2
 
 // lcd
-#define DISPLAY_RST_PIN B8
-#define DISPLAY_DC_PIN  B9
-#define DISPLAY_CS_PIN  C7
+#define DISPLAY_RST_PIN B13
+#define DISPLAY_DC_PIN B12
+#define DISPLAY_CS_PIN B9
+#define DISPLAY_SPI_DIVIDER 4
 
 #if defined(OLED_ENABLE) && defined(OLED_TRANSPORT_SPI)
 #define OLED_RST_PIN     DISPLAY_RST_PIN
 #define OLED_DC_PIN      DISPLAY_DC_PIN
 #define OLED_CS_PIN      DISPLAY_CS_PIN
-#define OLED_SPI_DIVISOR 2
+#define OLED_SPI_DIVISOR DISPLAY_SPI_DIVIDER
 #endif
