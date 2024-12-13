34f1421a099d6dfed3dcd0019ca05bc2e23aeed5
Drashna Jael're <drashna@live.com>
2024-12-13 13:14:56 -0800

    Fix unimportant erros"

--- quantum/action.c
+++ quantum/action.c
@@ -310,7 +310,7 @@ void process_record(keyrecord_t *record) {
 
 void process_record_handler(keyrecord_t *record) {
 #if defined(COMBO_ENABLE) || defined(REPEAT_KEY_ENABLE)
-    action_t action;
+    action_t action = {0};
     if (record->keycode) {
         action = action_for_keycode(record->keycode);
     } else {
@@ -1156,7 +1156,7 @@ bool is_tap_record(keyrecord_t *record) {
     }
 
 #if defined(COMBO_ENABLE) || defined(REPEAT_KEY_ENABLE)
-    action_t action;
+    action_t action = {0};
     if (record->keycode) {
         action = action_for_keycode(record->keycode);
     } else {
